b14e3b44f889defcbd5444a742d4befaf6ee68c2
n-ce <abrocks1120@gmail.com>
2021-09-19 15:26:18 +0530

    first stable build

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var b = document.querySelectorAll('h3');
+// displays newer content without moving pages
+
+var g = document.querySelectorAll('h3');
 var h = document.querySelectorAll('h1');
 
 function permutation(a,b,c,d){
-  h[a].style.display = "block";
+  h[a].style.display = "grid";
   h[b].style.display = 
   h[c].style.display = "none";
+  g[a].style.color ="#fff";
+  g[b].style.color = 
+  g[c].style.color ="#fff7";
   document.querySelector('header').innerHTML = d;
 }
+
